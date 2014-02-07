021c4340270526163a222e4245227abeb6a53a97
yangyang.cong <yangyang.cong@ttpod.com>
2014-02-07 16:29:54 +0800

    dir refactory

--- ttpod-vertx-groovy/src/main/java/com/ttpod/vertx/groovy/ext/VertxExtension.java
+++ ttpod-vertx-groovy/src/main/java/com/ttpod/vertx/groovy/ext/VertxExtension.java
@@ -9,7 +9,7 @@ import org.vertx.java.core.http.RouteMatcher;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Container;
 
-import java.lang.Object;import java.lang.String;import java.util.Map;
+import java.util.Map;
 
 /**
  * date: 14-1-26 下午5:38
@@ -18,12 +18,17 @@ import java.lang.Object;import java.lang.String;import java.util.Map;
  */
 public class VertxExtension {
 
-    public static void deployModule(Container container,String name, Map<String,Object> config) {
+    public static void DeployModule(Container container,String name, Map<String,Object> config) {
         container.deployModule(name,new JsonObject(config));
     }
 
 
-    public static void get(RouteMatcher route,String path,final Closure hanlder) {
+    public static void hello(Container container) {
+        System.out.println("   VertxExtension OK ..");
+    }
+
+
+    public static void Get(RouteMatcher route,String path,final Closure hanlder) {
         route.get(path,new Handler<HttpServerRequest>(){
             public void handle(HttpServerRequest event) {
                 hanlder.call(event);
@@ -32,7 +37,7 @@ public class VertxExtension {
     }
 
 
-    public static void send (EventBus bus,String address, Map<String,Object> msg,final Closure hanlder){
+    public static void Send (EventBus bus,String address, Map<String,Object> msg,final Closure hanlder){
         bus.send(address,new JsonObject(msg),new Handler<Message>() {
             public void handle(Message event) {
                 hanlder.call(event);
